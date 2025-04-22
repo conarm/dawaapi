c35dc339c3847dbb3f2bd5ada47326b317cbad78
Connor <conarm64@gmail.com>
2025-04-22 22:54:11 +0100

    Updated configs

--- svelte.config.js
+++ svelte.config.js
@@ -4,4 +4,13 @@ export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: dev ? '' : '/individual-project-conarm', // This must match your repo name
+    },
+    prerender: {
+      default: true
+    }
+  }
 };
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: "https://animated-couscous-9j34956.pages.github.io/"
+  base: '/individual-project-conarm/'
 });
